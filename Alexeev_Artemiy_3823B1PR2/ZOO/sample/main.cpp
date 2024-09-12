ed09c8b9f3eee8944206d25e92471afb1207e087
Induce <aa5619721@gmail.com>
2024-09-12 21:43:20 +0300

    12/09/24

--- Alexeev_Artemiy_3823B1PR2/ZOO/sample/main.cpp
+++ Alexeev_Artemiy_3823B1PR2/ZOO/sample/main.cpp
@@ -1,8 +1,4 @@
-#include"Animals.h"
+#include "Animals.h"
+
 int main(){
-    Animals* a;
-    a = new Cat("Steve","Persian",13,"home");
-    a->get_age();
-    a->get_place();
-    a->give_voice();
 }
